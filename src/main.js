6204c5cda6e9c0ceb51d9f9a91c26c4c9895ced2
Student <example@example.com>
2019-11-13 16:15:13 -0800

    practice using jest and unit testing

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { Triangle } from "./../triangle.js";
+
+$(document).ready(function(){
+  $("form#triangle").submit(function() {
+    event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    // if (side1 === side2 && side2 === side3 && side1 === side3) {
+    //   $('#test').show();
+    // }
+  });
+});
